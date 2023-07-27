98333def419064d8e8d0e80d91d0a53186ea0b7a
geoscrapper <91790678+geoscrapper@users.noreply.github.com>
2023-07-27 07:16:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -106,14 +106,14 @@ pip install bs4
 echo
 echo -e "\033[1;91m[*]\033[1;97m Setting up environment \033[0m"
 echo
-cp -r xosint $PREFIX/bin
+cp -r semurg $PREFIX/bin
 cp -r subdomains.txt $PREFIX/bin
-cd $PREFIX/bin && chmod u+x xosint
+cd $PREFIX/bin && chmod u+x semurg
 cd $PREFIX/bin && chmod u+x subdomains.txt
 cd $HOME
-rm -rf X-osint
-printf "\e[1;91m[*]\e[1;97m X-osint Installed Successfully, Now its ready for use. So re-open your Termux To use X-osint\e[0m\n" 
-printf "\e[1;91m[*]\e[1;97m After reopen your termux just type '\e[1;91mxosint\e[1;97m' to launch X-osint \e[0m" 
+rm -rf semurg
+printf "\e[1;91m[*]\e[1;97m Semurg Installed Successfully, Now its ready for use. So re-open your Termux To use Semurg\e[0m\n" 
+printf "\e[1;91m[*]\e[1;97m After reopen your termux just type '\e[1;91mxosint\e[1;97m' to launch Semurg \e[0m" 
 exit
  
  elif [[ $terminal == "02" || $terminal == "2" ]]; then
@@ -192,13 +192,13 @@ echo
 printf "\e[1;94mSetting up environment for Linux .. \e[0m\n"				
 sleep 1
 echo
-sudo cp -r xosint /usr/local/bin
-cd /usr/local/bin && sudo chmod u+x xosint
-cd $HOME && sudo rm -rf X-osint
- printf "\e[1;91m[*]\e[1;97m X-osint Installed Successfully, Now its ready for use. So re-open your Linux Terminal To use X-osint\e[0m\n"
+sudo cp -r semurg /usr/local/bin
+cd /usr/local/bin && sudo chmod u+x semurg
+cd $HOME && sudo rm -rf semurg
+ printf "\e[1;91m[*]\e[1;97m SemurgInstalled Successfully, Now its ready for use. So re-open your Linux Terminal To use semurg\e[0m\n"
  echo
  echo
- printf "\e[1;91m[*]\e[1;97m After reopen your terminal just type '\e[1;91msudo xosint\e[1;97m' to launch X-osint \e[0m" 
+ printf "\e[1;91m[*]\e[1;97m After reopen your terminal just type '\e[1;91msudo xosint\e[1;97m' to launch semurg \e[0m" 
  echo ""
  exit
 else 
